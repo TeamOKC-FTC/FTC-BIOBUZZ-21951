e4bb20c2c3fe0480a90ae1db9728f4dda5308f9a
Noah Zemlin <noah.zemlin@gmail.com>
2026-09-20 12:56:05 -0500

    Add example/default OpModes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpMode.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="TeleopOpMode")
+public class TeleopOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        // Initialize hardware and use hardwareMap
+
+
+        // Wait for user to press the Start button
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // Runs on repeat until user presses Stop button
+
+
+        }
+    }
+}
