b5cc20d9c66526f8b939b5033b6fccf6daf563b8
roylw <roy.l.wood@gmail.com>
2026-09-21 08:50:47 -0500

    test commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadExampleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadExampleOp.java
@@ -8,7 +8,7 @@ public class GamepadExampleOp extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        //test add for github
+        //test add for github dddd
         waitForStart();
 
         while (opModeIsActive()) {
